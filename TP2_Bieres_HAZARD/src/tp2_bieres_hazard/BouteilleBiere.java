b9642a2203f0c113b8a45baf84b011e58e15fe12
Romain Hazard <romai@10.3.63.3>
2025-11-03 15:30:45 +0100

    TP1 Exo 4 Statistiques

---
+++ TP2_Bieres_HAZARD/src/tp2_bieres_hazard/BouteilleBiere.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_hazard;
+
+/**
+ *
+ * @author romai
+ */
+public class BouteilleBiere {
+
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + "degres) \nBrasserie : " + brasserie);
+    }
+}
