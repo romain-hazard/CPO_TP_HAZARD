616aa928494e6c3cf10df6a1b08a19b6b45a6e71
Romain Hazard <romai@10.3.63.3>
2025-11-19 11:17:03 +0100

    TP3 Exo Relations

--- TP3_Heroic_Fantasy_HAZARD/src/Personnages/Personnage.java
+++ TP3_Heroic_Fantasy_HAZARD/src/Personnages/Personnage.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Personnages;
+import Armes.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,15 +13,49 @@ package Personnages;
 public abstract class Personnage {
     String nom; 
     int niveau_vie;
+    String Arme_en_Main="NULL";
 
     public Personnage(String nom, int niveau_vie) {
         this.nom = nom;
         this.niveau_vie = niveau_vie;
     }
     
+    
+    public void ArmeEq(Arme a){
+        for(int i=0; i<5; i++){
+            if(a.equals(armes.get(i))){
+                Arme_en_Main=armes.getNom(i);
+            }
+        }
+    }
+    
+    
+
+    public String getArme_en_Main() {
+        return Arme_en_Main;
+    }
+    
  
     @Override
     public String toString() {
         return "Le nom du persoi est "+nom+ " et son niveau de vie "+ niveau_vie;
-    }   
+    }  
+    
+    private ArrayList<Arme> armes = new ArrayList<>();
+
+    public void equiperArme(String nomArme) {
+    boolean trouve = false;
+
+    for (Arme a : armes) {
+        if (a.getNom().equals(nomArme)) {
+            Arme_en_Main = a;
+            System.out.println("Arme équipée : " + nomArme);
+            trouve = true;
+            break;
+        }
+    }
+    
+}
+
+    
 }
--- TP3_Heroic_Fantasy_HAZARD/src/tp3_heroic_fantasy_hazard/TP3_Heroic_Fantasy_HAZARD.java
+++ TP3_Heroic_Fantasy_HAZARD/src/tp3_heroic_fantasy_hazard/TP3_Heroic_Fantasy_HAZARD.java
@@ -20,41 +20,45 @@ public class TP3_Heroic_Fantasy_HAZARD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Epee Excalibure=new Epee("Excalibure",7,5);
-        Epee Durandal=new Epee("Duarandale",4,7);
-        Baton Chene=new Baton("Chene",4,5);
-        Baton Charme=new Baton("Charme",5,6);
-     
-        
-        
-        ArrayList<Arme>armes =new ArrayList<>();
+        Epee Excalibure = new Epee("Excalibure", 7, 5);
+        Epee Durandal = new Epee("Duarandale", 4, 7);
+        Baton Chene = new Baton("Chene", 4, 5);
+        Baton Charme = new Baton("Charme", 5, 6);
+
+        ArrayList<Arme> armes = new ArrayList<>();
         armes.add(Excalibure);
         armes.add(Durandal);
         armes.add(Chene);
         armes.add(Charme);
-        
-        
-        for (int i=0; i<armes.size(); i++){
+
+        for (int i = 0; i < armes.size(); i++) {
             System.out.println(armes.get(i));
         }
-        
-        Magicien Gandalf=new Magicien("Gandalf", 65, true);
-        Magicien Garcimore=new Magicien("Garcimore", 44, false);
-        Guerrier Conan=new Guerrier("Conan", 78, false);
-        Guerrier Lannister=new Guerrier("Lannister", 45, true);
-        
-        
-        ArrayList<Personnage>personnages =new ArrayList<>();
+
+        Magicien Gandalf = new Magicien("Gandalf", 65, true);
+        Magicien Garcimore = new Magicien("Garcimore", 44, false);
+        Guerrier Conan = new Guerrier("Conan", 78, false);
+        Guerrier Lannister = new Guerrier("Lannister", 45, true);
+
+        ArrayList<Personnage> personnages = new ArrayList<>();
         personnages.add(Gandalf);
         personnages.add(Garcimore);
         personnages.add(Conan);
         personnages.add(Lannister);
-        
-        
-        for (int i=0; i<armes.size(); i++){
+
+        for (int i = 0; i < armes.size(); i++) {
             System.out.println(armes.get(i));
         }
-        
+
+        ArrayList<Arme> inventaire = new ArrayList<>();
+        personnages.add(Gandalf);
+        personnages.add(Garcimore);
+        personnages.add(Conan);
+        personnages.add(Lannister);
+
+        for (int i = 0; i < armes.size(); i++) {
+            System.out.println(armes.get(i));
+
+        }
     }
-    
 }
