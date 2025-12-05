3e62b22ea98b3c7847f6fed4ebbed17565aa95d3
Romain Hazard <romai@10.3.63.3>
2025-12-05 14:54:05 +0100

    Projet Cadenas : - Ajout des éléments graphiques - Creation Classe "Combinaison" + constructeur, geter, seter

---
+++ Cadenas_HAZARD/src/Combinaison.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author romai
+ */
+public class Combinaison {
+    private int Chiffre1;
+    private int Chiffre2;
+    private int Chiffre3;
+    private int Chiffre4;
+
+    public Combinaison(int Chiffre1, int Chiffre2, int Chiffre3, int Chiffre4) {
+        this.Chiffre1 = Chiffre1;
+        this.Chiffre2 = Chiffre2;
+        this.Chiffre3 = Chiffre3;
+        this.Chiffre4 = Chiffre4;
+    }
+
+    public int getChiffre1() {
+        return Chiffre1;
+    }
+
+    public int getChiffre2() {
+        return Chiffre2;
+    }
+
+    public int getChiffre3() {
+        return Chiffre3;
+    }
+
+    public int getChiffre4() {
+        return Chiffre4;
+    }
+
+    public void setChiffre1(int Chiffre1) {
+        this.Chiffre1 = Chiffre1;
+    }
+
+    public void setChiffre2(int Chiffre2) {
+        this.Chiffre2 = Chiffre2;
+    }
+
+    public void setChiffre3(int Chiffre3) {
+        this.Chiffre3 = Chiffre3;
+    }
+
+    public void setChiffre4(int Chiffre4) {
+        this.Chiffre4 = Chiffre4;
+    }
+    
+    
+    
+    
+    
+    
+}
+
