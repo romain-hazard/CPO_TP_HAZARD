d4e9a82e0bb7ef03c45ef2e55c545cc33019140b
Romain Hazard <romai@10.3.63.3>
2025-12-17 10:57:03 +0100

    Threes Creation - Creation classe Grille, creation de la grille de jeu et gestion fusion des cases

---
+++ Threes_HAZARD/src/Grille.java
@@ -0,0 +1,60 @@
+
+import java.util.Random;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Romain
+ */
+public class Grille {
+  private int[][] grille;
+    private Random hasard;
+
+    public Grille() {
+        grille = new int[4][4];
+        hasard = new Random();
+        ajouterNombre();
+        ajouterNombre();
+    }
+
+    public int[][] getGrille() {
+        return grille;
+    }
+
+    public void ajouterNombre() {
+        int ligne, colonne;
+
+        do {
+            ligne = hasard.nextInt(4);
+            colonne = hasard.nextInt(4);
+        } while (grille[ligne][colonne] != 0);
+
+        if (hasard.nextBoolean()) {
+            grille[ligne][colonne] = 1;
+        } else {
+            grille[ligne][colonne] = 2;
+        }
+    }
+
+    public void fusion(int x1, int y1, int x2, int y2) {
+
+        int a = grille[x1][y1];
+        int b = grille[x2][y2];
+
+        if (a == 0 || b == 0) {
+            return;
+        }
+
+        if ((a == 1 && b == 2) || (a == 2 && b == 1)) {
+            grille[x1][y1] = 3;
+            grille[x2][y2] = 0;
+        } else if (a == b && a > 2) {
+            grille[x1][y1] = a * 2;
+            grille[x2][y2] = 0;
+        }
+    }   
+}
